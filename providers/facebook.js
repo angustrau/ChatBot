235a67be505056e51c7516cd1e06779ca14c7b09
Angus Trau <angus.amtra5@gmail.com>
2015-11-21 14:30:02 +1100

    Added facebook conversation join/leave messages

--- providers/facebook.js
+++ providers/facebook.js
@@ -38,21 +38,44 @@ exports.chatbotInit = function(chatRecieve) {
 
         console.log("[Facebook] Logged in as " + config.email);
 
-        //api.setOptions({"listenEvents":true, "updatePresence":true});
+        api.setOptions({"listenEvents":true, "updatePresence":true});
 
         stopListening = api.listen(function(err, message) {
             if (err) return console.log("[Facebook] Error in listen event");
 
             switch(message.type) {
                 case "message":
-                    recieveHandler(message.body, "text", getInfoTemplate(message), message.threadID)
+                    recieveHandler(message.body, "text", getInfoTemplate(message), message.threadID);
                     break;
                 case "presence":
-                    //offline messages are not consistent
+                    
                     break;
                 case "event":
+                    switch(message.logMessageType) {
+                        case "log:thread-name":
+                            break;
+                        case "log:subscribe":
+                            for (i=0;i<message.logMessageData.added_participants.length;i++) {
+                                var info = getInfoTemplate(message);
+                                info.name = message.logMessageBody.split(" added")[0];
+                                info.username = message.author;
+
+                                recieveHandler(message.logMessageData.added_participants[i].split(":")[1], "facebook_addedParticipant", info, message.threadID);
+                            }
+                            break;
+                        case "log:unsubscribe":
+                            for (i=0;i<message.logMessageData.removed_participants.length;i++) {
+                                var info = getInfoTemplate(message);
+                                info.name = message.logMessageBody.split(" removed")[0];
+                                info.username = message.author;
+    
+                                recieveHandler(message.logMessageData.removed_participants[0].split(":")[1], "facebook_removedParticipant", info, message.threadID);
+                            }
+                            break;
+                    }
+                    break;
                 case "typ":
-                    recieveHandler(message.isTyping, "facebook_isTyping", getInfoTemplate(message), message.threadID)
+                    recieveHandler(message.isTyping, "facebook_isTyping", getInfoTemplate(message), message.threadID);
                     break;
             }
         });
