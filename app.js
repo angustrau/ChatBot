d9cee75afcf334ff105460a727925dd535b1a3f5
Angus Trau <angus.amtra5@gmail.com>
2015-11-23 20:50:00 +1100

    provide providers for plugins

--- app.js
+++ app.js
@@ -2,11 +2,12 @@
 var path = require("path");
 var log = require("./logging.js")
 
-var providers = [];
+var providers = {};
 var plugins = [];
 var chats = [];
 
 function chatBroadcast(message) {
+    //non functional
     chats.forEach(function(callback) {
         callback(message); 
     });
@@ -15,10 +16,12 @@ function chatBroadcast(message) {
 function chatRecieve(message, type, info, id) {
     type = type || "text";
 
+    info.provider = providers[info.service];
+
     log.info("Recieved message '" + message + "' of type '" + type + "', id: " + id, info.service);
 
     plugins.forEach(function(plugin) {
-        if (plugin.chatbotEvent) plugin.chatbotEvent(message, type, info, id);
+        if (plugin.chatbotEvent) plugin.chatbotEvent(message, type, info, id); //Called when a message is recieved
     });
 }
 
@@ -38,9 +41,9 @@ providerFiles.forEach(function(filename) {
     log.info("Loading " + filename + "...", "ChatBot");
     var provider = require("./providers/" + filename);
 
-    if (provider.chatbotInit) provider.chatbotInit(chatRecieve);
+    if (provider.chatbotInit) provider.chatbotInit(chatRecieve); //Called on init. Passes a callback for when a message is recieved
 
-    providers.push(provider);
+    providers[provider.name] = provider;
 
     log.info("Loaded " + filename, "ChatBot");
 });
@@ -54,7 +57,8 @@ pluginFiles.forEach(function(filename) {
      log.info("Loading " + filename + "...", "ChatBot");
      var plugin = require("./plugins/" + filename);
 
-     if (plugin.chatbotInit) plugin.chatbotInit(chatBroadcast);
+     if (plugin.chatbotInit) plugin.chatbotInit(chatBroadcast); //Called on init. Passes a callback to broadcast a message to all open chats
+     if (plugin.chatbotProviders) plugin.chatbotProviders(providers); //Passes providers to a plugin if requested
 
      plugins.push(plugin);
 
